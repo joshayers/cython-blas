5281aeebf1714bf337f751b314d274dcdefb642e
Josh Ayers <joayers@relativityspace.com>
2025-05-20 11:15:36 -0700

    Added a script for the repair-wheel command on Windows.

--- tools/before_build.py
+++ tools/before_build.py
@@ -1,3 +1,5 @@
+"""Commands to run before build."""
+
 import os
 from pathlib import Path
 
@@ -5,7 +7,9 @@ import scipy_openblas64
 
 
 def main() -> None:
+    """Main."""
     path = Path(os.environ["PKG_CONFIG_PATH"]) / "scipy-openblas.pc"
+    print(f"Writing file: {path!s}")
     with path.open("wt") as fobj:
         fobj.write(scipy_openblas64.get_pkg_config())
 
