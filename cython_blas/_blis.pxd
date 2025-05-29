fe321860f4a4061748a37d5b6e1378e36a3d39bc
Josh Ayers <josh@ayers.pw>
2025-05-28 19:14:19 -0700

    BLIS dgemm appears to be working.

--- cython_blas/_blis.pxd
+++ cython_blas/_blis.pxd
@@ -1,9 +1,60 @@
 
 cdef extern from "blis.h" nogil:
 
-    ctypedef int arch_t
+    ctypedef int dim_t  # dimension type
+    ctypedef int inc_t  # increment/stride type
+
+    ctypedef int arch_t  # architecture ID type
+
+    cdef enum num_t:
+        BLIS_FLOAT
+        BLIS_DOUBLE
+        BLIS_SCOMPLEX
+        BLIS_DCOMPLEX
+
+    cdef enum dom_t:
+        BLIS_REAL
+        BLIS_COMPLEX
+
+    cdef enum trans_t:
+        BLIS_NO_TRANSPOSE
+        BLIS_TRANSPOSE
+        BLIS_CONJ_NO_TRANSPOSE
+        BLIS_CONJ_TRANSPOSE
+
+    cdef enum conj_t:
+        BLIS_NO_CONJUGTE
+        BLIS_CONJUGATE
+
+    ctypedef struct obj_t: # matrix object
+        pass
+
 
     const char* bli_info_get_int_type_size_str()
     const char* bli_info_get_version_str()
     arch_t bli_arch_query_id()
     const char* bli_arch_string(arch_t id)
+
+    void bli_obj_create_with_attached_buffer(
+        num_t   dt,  # type
+        dim_t   m,   # number of rows
+        dim_t   n,   # number of columns
+        void*   p,   # buffer
+        inc_t   rs,  # row stride
+        inc_t   cs,  # column stride
+        obj_t*  obj  # matrix object initialized
+    )
+
+    void bli_obj_create_1x1_with_attached_buffer(
+        num_t   dt,  # type
+        void*   p,   # buffer
+        obj_t*  obj, # matrix object initialized
+    )
+
+    void bli_gemm(
+        const obj_t*  alpha,
+        const obj_t*  a,
+        const obj_t*  b,
+        const obj_t*  beta,
+        obj_t*  c,
+    )
--- cython_blas/blis.pyx
+++ cython_blas/blis.pyx
@@ -1,5 +1,8 @@
 
+cimport cython
+
 from cython_blas cimport _blis
+from cython_blas._blis cimport bli_gemm
 
 
 def get_int_type_size():
@@ -19,3 +22,54 @@ def get_arch():
     cdef const char* cstring = _blis.bli_arch_string(id)
     cdef bytes bstring = cstring
     return bstring.decode('ascii')
+
+
+@cython.cdivision(True)
+def dgemm(
+    double alpha,
+    const double [:, :] A,
+    const double [:, :] B,
+    double beta,
+    double [:, :] C
+):
+    cdef _blis.obj_t bli_a, bli_b, bli_c, bli_alpha, bli_beta
+
+    _blis.bli_obj_create_1x1_with_attached_buffer(
+        _blis.BLIS_DOUBLE,
+        &alpha,
+        &bli_alpha,
+    )
+    _blis.bli_obj_create_with_attached_buffer(
+        _blis.BLIS_DOUBLE,
+        A.shape[0],
+        A.shape[1],
+        <void*> &A[0, 0],
+        A.strides[0] // sizeof(double),
+        A.strides[1] // sizeof(double),
+        &bli_a,
+    )
+    _blis.bli_obj_create_with_attached_buffer(
+        _blis.BLIS_DOUBLE,
+        B.shape[0],
+        B.shape[1],
+        <void*> &B[0, 0],
+        B.strides[0] // sizeof(double),
+        B.strides[1] // sizeof(double),
+        &bli_b,
+    )
+    _blis.bli_obj_create_1x1_with_attached_buffer(
+        _blis.BLIS_DOUBLE,
+        &beta,
+        &bli_beta,
+    )
+    _blis.bli_obj_create_with_attached_buffer(
+        _blis.BLIS_DOUBLE,
+        C.shape[0],
+        C.shape[1],
+        <void*> &C[0, 0],
+        C.strides[0] // sizeof(double),
+        C.strides[1] // sizeof(double),
+        &bli_c,
+    )
+
+    bli_gemm(&bli_alpha, &bli_a, &bli_b, &bli_beta, &bli_c)
