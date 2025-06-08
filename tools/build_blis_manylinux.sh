7ee52672a31da424ed60a2c8a95ef02729238497
Josh Ayers <josh@ayers.pw>
2025-06-08 10:24:47 -0700

    Replace Linux library with version compiled with manylinux container.

--- tools/build_blis_manylinux.sh
+++ tools/build_blis_manylinux.sh
@@ -2,22 +2,21 @@
 
 SRC_DIR="$1"
 
+# Copy cython-blas/vendor/blis to container
 mkdir -p /project/blis
 echo "/host$SRC_DIR"
 cp -r /host$SRC_DIR /project
 cd /project/blis
 
-echo "Directory contents:"
-ls -la
-
+# Configure and build
 export CONFIGURE_OPTS="$CONFIGURE_OPTS --disable-shared --enable-static"
 export CONFIGURE_OPTS="$CONFIGURE_OPTS --disable-blas"
 export CONFIGURE_OPTS="$CONFIGURE_OPTS --disable-cblas"
 export CONFIGURE_OPTS="$CONFIGURE_OPTS --enable-threading=openmp"
-
 ./configure --prefix=/opt/blis $CONFIGURE_OPTS --enable-arg-max-hack x86_64
 make -j2 V=1
 make install
 
+# Copy compiled library back to host
 mkdir -p /host/opt/blis
 cp -r /opt/blis /host/opt/blis
