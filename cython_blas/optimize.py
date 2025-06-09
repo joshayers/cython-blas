c6c0be0cda7a502c2c2f7424ea8c8087a41cb54b
Josh Ayers <josh@ayers.pw>
2025-06-08 19:10:35 -0700

    Added tests of optimize module.

--- cython_blas/optimize.py
+++ cython_blas/optimize.py
@@ -7,32 +7,48 @@ Matrix = TypeVar("Matrix", bound="Matrix")
 MultiMatrix = TypeVar("MultiMatrix", bound="MultiMatrix")
 
 
+kind_to_weight = {
+    ("f", "f", "f"): 2,
+    ("f", "f", "c"): 2,
+    ("f", "c", "f"): 2,
+    ("f", "c", "c"): 4,
+    ("c", "f", "f"): 2,
+    ("c", "f", "c"): 4,
+    ("c", "c", "f"): 4,
+    ("c", "c", "c"): 8,
+}
+
+
 class Matrix:
     """Matrix class."""
 
-    def __init__(self, shape: tuple, dtype: str) -> None:
+    def __init__(self, shape: tuple[int, int], kind: str, itemsize: int) -> None:
         """Initialize the class."""
         self.shape = shape
-        self.dtype = dtype
+        self.kind = kind
+        self.multiplier = itemsize // 4
 
-    def __eq__(self, other: Matrix | MultiMatrix) -> bool:
-        """Check for equality."""
+    def __eq__(self, other: Matrix) -> bool:
+        """Check equality."""
         if self.shape != other.shape:
             return False
-        return self.dtype == other.dtype
+        if self.kind != other.kind:
+            return False
+        return self.multiplier == other.multiplier
 
-    def calc_flops(self, other: Matrix | MultiMatrix) -> tuple[int, Matrix | MultiMatrix]:
-        """Calculate the flops due to multiplying two matrices."""
+    def calc_weight(
+        self, other: Matrix | MultiMatrix, output_kind: str, output_itemsize: int
+    ) -> tuple[int, Matrix | MultiMatrix]:
+        """Calculate the weight due to multiplying two matrices."""
         if isinstance(other, MultiMatrix):
-            return other.calc_flops_left(self)
-        dtype_mult = {"f4": 1, "f8": 2, "c8": 2, "c16": 4}
-        mult = max(dtype_mult[self.dtype], dtype_mult[other.dtype])
+            return other.calc_weight_left(self)
+        mult = output_itemsize // 4 * kind_to_weight[(output_kind, self.kind, other.kind)]
         m, k = self.shape
         k2, n = other.shape
         if k != k2:
             raise ValueError
-        flops = mult * m * k * n
-        return flops, Matrix((m, n), self.dtype)
+        weight = mult * m * k * n
+        return weight, Matrix((m, n), output_kind, output_itemsize)
 
 
 class MultiMatrix:
@@ -42,37 +58,46 @@ class MultiMatrix:
         """Initialize the class."""
         self.mats = mats
 
-    def _calc_flops_multimatrix(self, other: MultiMatrix) -> tuple[int, MultiMatrix]:
-        """Calculate the flops when multiplying two MultiMatrix instances."""
-        flops = 0
+    @property
+    def n_mats(self) -> int:
+        """Return the number of matrices."""
+        return len(self.mats)
+
+    def _calc_weight_multimatrix(
+        self, other: MultiMatrix, output_kind: str, output_itemsize: int
+    ) -> tuple[int, MultiMatrix]:
+        """Calculate the weight when multiplying two MultiMatrix instances."""
+        weight = 0
         mats = []
         for mat1, mat2 in zip(self.mats, other.mats, strict=True):
-            flops_i, mats_i = mat1.calc_flops(mat2)
-            flops += flops_i
+            weight_i, mats_i = mat1.calc_weight(mat2, output_kind, output_itemsize)
+            weight += weight_i
             mats.append(mats_i)
-        return flops, MultiMatrix(mats)
+        return weight, MultiMatrix(mats)
 
-    def calc_flops_left(self, other: Matrix) -> tuple[int, MultiMatrix]:
-        """Calculate the flops for other @ self."""
-        flops = 0
+    def calc_weight_left(self, other: Matrix, output_kind: str, output_itemsize: int) -> tuple[int, MultiMatrix]:
+        """Calculate the weight for other @ self."""
+        weight = 0
         mats = []
         for mat2 in self.mats:
-            flops_i, mats_i = other.calc_flops(mat2)
-            flops += flops_i
+            weight_i, mats_i = other.calc_weight(mat2, output_kind, output_itemsize)
+            weight += weight_i
             mats.append(mats_i)
-        return flops, MultiMatrix(mats)
+        return weight, MultiMatrix(mats)
 
-    def calc_flops(self, other: Matrix | MultiMatrix) -> tuple[int, Matrix | MultiMatrix]:
-        """Calculate the flops for self @ other."""
+    def calc_weight(
+        self, other: Matrix | MultiMatrix, output_kind: str, output_itemsize: int
+    ) -> tuple[int, Matrix | MultiMatrix]:
+        """Calculate the weight for self @ other."""
         if isinstance(other, MultiMatrix):
-            return self._calc_flops_multimatrix(other)
-        flops = 0
+            return self._calc_weight_multimatrix(other, output_kind, output_itemsize)
+        weight = 0
         mats = []
         for mat1 in self.mats:
-            flops_i, mats_i = mat1.calc_flops(other)
-            flops += flops_i
+            weight_i, mats_i = mat1.calc_weight(other, output_kind, output_itemsize)
+            weight += weight_i
             mats.append(mats_i)
-        return flops, MultiMatrix(mats)
+        return weight, MultiMatrix(mats)
 
 
 def optimize(mats: list[Matrix]) -> tuple[int, int, tuple[int]]:
@@ -81,12 +106,12 @@ def optimize(mats: list[Matrix]) -> tuple[int, int, tuple[int]]:
     paths = itertools.product(*(range(i) for i in range(n_mats - 1, 0, -1)))
     best_path = (-1, 2**63, ())
     for i, path in enumerate(paths):
-        flops = 0
+        weight = 0
         mats_i = list(mats)
         for pair in path:
-            flops_i, remaining = mats_i[pair].calc_flops(mats_i[pair + 1])
-            flops += flops_i
+            weight_i, remaining = mats_i[pair].calc_weight(mats_i[pair + 1])
+            weight += weight_i
             mats_i = [*mats_i[:pair], remaining, *mats_i[pair + 2 :]]
-        if flops < best_path[1]:
-            best_path = (i, flops, path)
+        if weight < best_path[1]:
+            best_path = (i, weight, path)
     return best_path
