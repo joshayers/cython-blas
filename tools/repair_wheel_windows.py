6707f47a7eefe166ec8c1647a3289de7d660ea94
Josh Ayers <josh@ayers.pw>
2025-07-04 20:38:47 -0700

    Squashed commit of the following:
    commit b0d9d37ca43e1def31e241addfc8182841909410
    Author: Josh Ayers <josh@ayers.pw>
    Date:   Fri Jul 4 20:36:15 2025 -0700
    
        Removed Python 3.10 tests.
    
    commit aadddc40c7eb1a8465aedd3d232de6ad16e5662c
    Author: Josh Ayers <josh@ayers.pw>
    Date:   Fri Jul 4 20:27:56 2025 -0700
    
        Updated.
    
    commit 561134d724e2274a28f1d527b45f583dfae624a6
    Author: Josh Ayers <josh@ayers.pw>
    Date:   Fri Jul 4 20:22:04 2025 -0700
    
        Try linux script again.
    
    commit 1feb1a21fab9c76f310b8fa543669350be71705c
    Author: Josh Ayers <josh@ayers.pw>
    Date:   Fri Jul 4 19:38:11 2025 -0700
    
        Try again.
    
    commit dbd091f45390121c7ee685c79fc29ebef03bb127
    Author: Josh Ayers <josh@ayers.pw>
    Date:   Fri Jul 4 19:32:33 2025 -0700
    
        Install abi3audit in before-build on Linux.
    
    commit 47d0f001af3d2ba5124f8832cfd6f84ad2c5879b
    Author: Josh Ayers <josh@ayers.pw>
    Date:   Fri Jul 4 19:22:50 2025 -0700
    
        Try building wheels using limited api.
    
    commit 70fe193c1e6ea912e85d50eadaa98a64a96b7428
    Author: Josh Ayers <josh@ayers.pw>
    Date:   Fri Jul 4 16:58:05 2025 -0700
    
        Added limited-api flag in pyproject.toml.
    
    commit 692618c0fb3f0f6a5df186173268e2b14fa11c89
    Author: Josh Ayers <josh@ayers.pw>
    Date:   Fri Jul 4 16:22:21 2025 -0700
    
        Attempt to use limited api.

--- tools/repair_wheel_windows.py
+++ tools/repair_wheel_windows.py
@@ -10,6 +10,9 @@ def main() -> None:
     """Main."""
     dest_dir = sys.argv[1]
     wheel = sys.argv[2]
+
+    # repair wheel with delvewheel
+    # need to explicitly add OpenBLAS's lib dir because it's not on the default dll search path
     lib_dir = scipy_openblas64.get_lib_dir()
     cmd = ["delvewheel", "repair", f"--add-path={lib_dir!s}", f"--wheel-dir={dest_dir!s}", f"{wheel!s}"]
     print("Running command:")
